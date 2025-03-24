f2a552421a161cee95c92b217e9b2b7de94cc814
Tjs87474 <Tsingleton14@student.gsu.edu>
2025-03-24 18:23:55 -0400

    Added confirmation screen for onpressed

---
+++ lib/confirmation.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class Confirmation extends StatefulWidget {
+  @override
+  _ConfirmationState createState() => _ConfirmationState();
+}
+
+class _ConfirmationState extends State<Confirmation> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Welcome'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Welcome! You have successfully created your account.',
+              textAlign: TextAlign.center,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
